f52c6a9c16b3ce2893f3b40ff975c0615e983146
Terry Cheung <terrycyc812@gmail.com>
2020-08-25 11:46:15 +0800

    Firstly created

---
+++ DailyChallenge1.py
@@ -0,0 +1,11 @@
+# Create a function that removes all capital letters and punctuation in a string. Return the clean string.
+
+import string
+def clean_string(s):
+    a = str()
+    for i in s:
+        if i.isupper() == False and i not in string.punctuation:
+            a += i
+    return a
+
+print(clean_string("Abc.De!@f13J"))
